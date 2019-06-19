f0d34e7e3b5d39c8b18ea3e0a1cfc799a977c4ad
nmamidon <nmamidon@gmail.com>
2019-06-18 22:22:29 -0400

    changing bawg description to what Milner sent me

--- src/components/Bawg.js
+++ src/components/Bawg.js
@@ -6,15 +6,12 @@ const Bawg = () => {
 		<div className="flex flex-wrap flex-nowrap-l">
 			<div className="mr4 w-100">
 				<h1 className="baskerville bb b--moon-gray"><i>BAWG</i></h1>
-				<p>The BREAKFAST ARCHIVES WORKING GROUP (BAWG) makes collaborative zines that collect all kinds of visual art and written material relating to THE MACHINERY OF EVERYDAY LIFE -- THE PRACTICES THAT HOLD OUR COMMUNITIES TOGETHER AND THUS MAKE THE WORLD MORE BEARABLE -- THINGS WIDELY CONSIDERED TOO COMMONPLACE TO FLIP OUT OVER BUT ARE NEVERTHELESS WORTHY OF CELEBRATION AND DOCUMENTATION -- </p>
-	        	<p>When we say breakfast, [we're not necessarily being literal]1. But, in some sense we are, because breakfast is generally a great time to meet up with your friends, plan, bond, recuperate, take care of each other -- you know, all stuff we're into. We welcome submissions from anyone. Whatever you're submitting, we've got an exceptional love for the experimental, the exuberant, the unpolished, the risky, the mildly incomprehensible (but if you want to say a few words explaining what you've made, that's cool too). The specifics of what we're looking for depend on the themes of the issue -- read more about those below!</p>
-	        	<p>Upcoming issues:</p>
-	        	<ul className="list">
-	        		<li>#1: [Get Hype For The Insignificant!]2 (SUBMISSIONS OPEN: fb event [here]3; submit your art to dead.army.choir@gmail.com by 2/17/19)</li>
-	        		<li>#2: Hallowed Sites/Lost Spaces</li>
-	        		<li>#3: Eggs</li>
-	        	</ul>
-	        	<p>Got any ideas for something else you'd like to see in print? Want to band together and make more stuff with us under the Bummer City banner? Shoot us an email.</p>
+				<p>WHAT IS BREAKFAST??</p>
+				<p>BREAKFAST is one of society's many unsung engines -- it is all the little things we do, public or private, that structure our day -- it is unpretentious -- it is intimate -- it is adaptable -- BREAKFAST must be defended in a world where time off the clock is considered a luxury good.</p>
+				<p>THE BREAKFAST ARCHIVES WORKING GROUP is a zine collective that aims to celebrate THE MACHINERY OF EVERYDAY LIFE through our very own arts + literary journal, BREAKFAST ARCHIVES.</p>
+				<p>Our thematic directions vary widely from issue to issue -- our driving principles include effusive commemoration (aka getting hype), as well as camaraderie. we hate commodification. we love public spaces -- willful co-optation -- intentionality, hash browns. we're not necessarily being literal.</p>
+				<p>WE ARE CURRENTLY TAKING SUBMISSIONS FOR OUR SECOND ISSUE! submission details/prompts can be found here --> <a href="https://cooltapes.itch.io/bawg2">cooltapes.itch.io/bawg2</a></p>
+				<p>download free digital copies of our past issues here --> <a href="https://cooltapes.itch.io/bawg">cooltapes.itch.io/bawg</a></p>
 	        </div>
 	        <Sidebar />
         </div>
