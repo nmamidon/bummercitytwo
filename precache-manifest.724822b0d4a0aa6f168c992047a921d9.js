47835b50252ed6346dab84e9ff5f226985398927
nmamidon <nmamidon@gmail.com>
2019-08-14 11:40:04 -0400

    Updates

---
+++ precache-manifest.724822b0d4a0aa6f168c992047a921d9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff392ec9caa1a31d7349c530d28619e0",
+    "url": "/bummercitytwo/index.html"
+  },
+  {
+    "revision": "5e066d84974e64939ab5",
+    "url": "/bummercitytwo/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "458912d1a706b424abca",
+    "url": "/bummercitytwo/static/css/main.01d352e3.chunk.css"
+  },
+  {
+    "revision": "5e066d84974e64939ab5",
+    "url": "/bummercitytwo/static/js/2.4e537096.chunk.js"
+  },
+  {
+    "revision": "458912d1a706b424abca",
+    "url": "/bummercitytwo/static/js/main.6945eacf.chunk.js"
+  },
+  {
+    "revision": "92cad8a81e7a7bb3045c",
+    "url": "/bummercitytwo/static/js/runtime~main.0b2c4a54.js"
+  },
+  {
+    "revision": "f5ad31714fb1d28403411f292c3d2729",
+    "url": "/bummercitytwo/static/media/BCHSbanner.f5ad3171.png"
+  },
+  {
+    "revision": "308ba1b2c86dc48108dc94e0a8ae4410",
+    "url": "/bummercitytwo/static/media/bawg-printed-everything.308ba1b2.jpg"
+  },
+  {
+    "revision": "c0cc81d6ec9d2947e80886b23ef7760d",
+    "url": "/bummercitytwo/static/media/bcbcamanda.c0cc81d6.jpg"
+  },
+  {
+    "revision": "6aed77609ca67bc54cad0aba79825ff9",
+    "url": "/bummercitytwo/static/media/cheese_boys_open_mic.6aed7760.jpg"
+  },
+  {
+    "revision": "aae018481e6ed926047789609604e704",
+    "url": "/bummercitytwo/static/media/civic_engagement_and_matt.aae01848.jpg"
+  },
+  {
+    "revision": "489ddbb40db176c531af1fb8e81fecf7",
+    "url": "/bummercitytwo/static/media/jimmy_and_lou.489ddbb4.jpg"
+  },
+  {
+    "revision": "7c29933c0ef5ca5fa631535693aa1ce2",
+    "url": "/bummercitytwo/static/media/kelly_house_show.7c29933c.jpg"
+  },
+  {
+    "revision": "4ba2d681fcb12044b03dc1f9fe4f3c28",
+    "url": "/bummercitytwo/static/media/kona.4ba2d681.jpg"
+  },
+  {
+    "revision": "9c5bd599bb24c3291dedd886e2438d1f",
+    "url": "/bummercitytwo/static/media/legsnacks.9c5bd599.jpeg"
+  },
+  {
+    "revision": "e2bd126e60cb2b23e91fc0d8900ef4fb",
+    "url": "/bummercitytwo/static/media/marty_and_fam.e2bd126e.jpg"
+  },
+  {
+    "revision": "b309dd185344d112cd3a595cda1e1668",
+    "url": "/bummercitytwo/static/media/matt.b309dd18.jpg"
+  },
+  {
+    "revision": "285465bbf44f5ffa18eec936d77ae435",
+    "url": "/bummercitytwo/static/media/mcproffy1.285465bb.jpg"
+  },
+  {
+    "revision": "8c30329832177555df207ff5559339b4",
+    "url": "/bummercitytwo/static/media/wafflecaper.8c303298.jpg"
+  }
+]);
