7572955077366720a14fa334badb7405e6579a38
nmamidon <nmamidon@gmail.com>
2020-10-25 09:49:53 -0400

    added 2020 voting guide to Elections page

--- src/components/Elections.js
+++ src/components/Elections.js
@@ -11,6 +11,7 @@ const Elections = () => {
 		        	<h3>2020</h3>
 		        	<p><em>September 1st, 2020</em> -- Primary Congressional Elections (Massachusetts)</p>
 		        	<p><em>November 3, 2020</em> -- General Presidential Election</p>
+		        		<li><a href="https://docs.google.com/document/d/1WUTHqUi_ahtz8182K7kuM_sINsGCt0TBYmNQ9hoJC5Q/edit"><b>2020 VOTING GUIDE</b></a></li> 
 		        	<h2 className="baskerville pt3">Past Elections</h2>
 		        	<h3>2019</h3>
 		        	<p><em>November 5th, 2019</em> -- State & Local Off-Year Elections</p>
@@ -27,6 +28,7 @@ const Elections = () => {
 	         	<div className="mv4"> 
 		         	<h1 className="baskerville bb b--moon-gray"><i>Voting Guides</i></h1>
 		         	<p> Sometimes you don't have time to do all of the research on your own, and that's okay! For those of you who are not entirely sure who you want to vote for, who are looking for some guidance, here are our suggestions. These are in no way authoritative; they are merely the endorsements that the Bummer City team landed on after deliberation.</p>
+		         	<li><a href="https://docs.google.com/document/d/1WUTHqUi_ahtz8182K7kuM_sINsGCt0TBYmNQ9hoJC5Q/edit"><b>2020 General Presidential Voting Guide</b></a></li> 
 		            <li> <a href="https://drive.google.com/file/d/0B1JroPKomfUdbW1TU1BWOEhhZFl6a2JYWHZRc2dlSXFJVzZr/view?usp=sharing"><b>2019 State & Local Off-Year Election Voting Guide</b></a></li> 
 		            <li> <a href="https://docs.google.com/document/d/1G-7HkrfoJ8RjGRJ8XHIASY_6N_ABRigvWwtKiN7VlZU/edit?usp=sharing"><b>2018 Midterm General Election Voting Guide</b></a></li> 
 		            <li> <a href="https://docs.google.com/document/d/19jWRvtjLr2kAYmBvc7PzJEzd8jPZp1EmwpeSiaKI7GQ/edit?usp=sharing"><b>2018 MA Primary Voting Guide</b></a></li> 
